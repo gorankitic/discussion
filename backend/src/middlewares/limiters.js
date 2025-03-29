6e843f87b38f94a34838dedb49a2cfe14c6930f7
Goran Kitic <kiticgoran90@gmail.com>
2025-03-29 18:53:14 +0100

    Modified limit in limiters middleware 🚀

--- backend/src/middlewares/limiters.js
+++ backend/src/middlewares/limiters.js
@@ -5,7 +5,7 @@ const AppError = require("../lib/AppError");
 
 exports.loginLimiter = rateLimiter({
     windowMs: 30 * 60 * 1000, // 30 minutes
-    limit: 5, // Limit each IP to 10 requests per `window` (here, per 30 minutes)
+    limit: 5, // Limit each IP to 5 requests per `window` (here, per 30 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
     message: "Too many login attempts from this IP address. Try again in 30 minutes.",
@@ -14,7 +14,7 @@ exports.loginLimiter = rateLimiter({
 
 exports.limiter = rateLimiter({
     windowMs: 30 * 60 * 1000, // 30 minutes
-    limit: 100, // Limit each IP to 100 requests per `window` (here, per 30 minutes)
+    limit: 200, // Limit each IP to 200 requests per `window` (here, per 30 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
     message: "Too many login attempts from this IP address. Try again in 30 minutes.",
